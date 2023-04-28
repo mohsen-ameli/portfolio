066ba567d057f7834bb4287748931fcb94e998fc
mohsen-ameli <mohsen.ameli83@gmail.com>
2023-04-28 01:47:31 -0400

    fixed window location issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,18 @@
 
 import { Node } from "@/types/main.type"
 import { AnimatePresence, motion } from "framer-motion"
+import { useEffect, useState } from "react"
 
 const Layout = ({ children }: Node) => {
+  const [path, setPath] = useState<string>()
+
+  useEffect(() => {
+    setPath(window.location.pathname)
+  }, [])
+
   return (
     <AnimatePresence mode="wait">
-      <motion.div key={location.pathname}>{children}</motion.div>
+      <motion.div key={path}>{children}</motion.div>
     </AnimatePresence>
   )
 }
